e706f70151c6a9d2aca2bfccd4f3f7aecd00722c
Omar Meriwani <omar.sirwan@gmail.com>
2020-05-08 00:05:36 +0100

    Finish file readers

--- ReadFolder.py
+++ ReadFolder.py
@@ -1,3 +1,16 @@
 from os import listdir
 from os.path import isfile, join
 
+mypath = 'E:/dathena-competition/files/Keppel'
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+#print(onlyfiles)
+
+def GetFilesInFolder(path):
+    onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+    for file in onlyfiles:
+        fileS = file.split('.')
+        fileExt = fileS[len(fileS) - 1]
+        print(fileExt)
+
+
+#GetFilesInFolder(mypath)
